6527cb225f0093dc6ef3447967788ced0060e3e8
wsrn829 <wsrn829@gmail.com>
2024-01-11 23:25:59 -0500

    Move inventory_rest

---
+++ inventory_rest/encoders.py
@@ -0,0 +1,39 @@
+from common.json import ModelEncoder
+
+from .models import Automobile, Manufacturer, VehicleModel
+
+
+class ManufacturerEncoder(ModelEncoder):
+    model = Manufacturer
+    properties = [
+        "id",
+        "name",
+    ]
+
+
+class VehicleModelEncoder(ModelEncoder):
+    model = VehicleModel
+    properties = [
+        "id",
+        "name",
+        "picture_url",
+        "manufacturer",
+    ]
+    encoders = {
+        "manufacturer": ManufacturerEncoder(),
+    }
+
+
+class AutomobileEncoder(ModelEncoder):
+    model = Automobile
+    properties = [
+        "id",
+        "color",
+        "year",
+        "vin",
+        "model",
+        "sold",
+    ]
+    encoders = {
+        "model": VehicleModelEncoder(),
+    }
