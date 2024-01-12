13502e35f2149e804dd21af983b6bbb49d0e2d2a
wsrn829 <wsrn829@gmail.com>
2024-01-11 22:34:08 -0500

    Update collectstatic

--- inventory/api/inventory_project/settings.py
+++ inventory/api/inventory_project/settings.py
@@ -22,7 +22,7 @@ from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
