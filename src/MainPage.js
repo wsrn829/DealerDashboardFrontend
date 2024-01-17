e043ef15ad00d8b211fdb5f7221743042fcd9f8c
wsrn829 <wsrn829@gmail.com>
2024-01-17 16:54:33 -0500

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,9 +3,9 @@ import React from 'react';
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <h1 className="display-5 fw-bold">DealerDashboard</h1>
       <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
+        <p className="lead mb-6">
           The premiere solution for automobile dealership
           management!
         </p>
